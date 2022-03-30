496bc54fa989720f1899a6aacd543101fddb53ac
Jeremy Junac <jeremy.junac@amadeus.com>
2022-03-30 11:57:35 +0200

    Function code for Challenge 3

--- RatingFunc/__init__.py
+++ RatingFunc/__init__.py
@@ -1,16 +1,69 @@
 import logging
+import time
+import uuid
 from typing import Optional
 import azure.functions as func
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as cosmos_exceptions
 
-from fastapi import FastAPI
+from azure.cosmos.partition_key import PartitionKey
+
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+DB_HOST = "https://openhackdb.documents.azure.com:443/"
+DB_MASTER_KEY = "GYslGRpMOdHh9eHfR412sV4Cr5UU3DgeL8jHQ9pLnYXUChGw3GBFCExnWwxTOW3SwqVgmLxyR243TjCfJTbSUQ=="
+DB_DATABASE_ID = "rating"
+DB_CONTAINER_ID = "ratingcontainer"
+
+db_client = cosmos_client.CosmosClient(DB_HOST, {'masterKey': DB_MASTER_KEY} )
+rating_container = db_client.create_database_if_not_exists(id=DB_DATABASE_ID).create_container_if_not_exists(id=DB_CONTAINER_ID, partition_key=PartitionKey(path='/id', kind='Hash'))
 
 app = FastAPI()
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.AsgiMiddleware(app).handle(req, context)
 
+class Rating(BaseModel):
+    userId: str
+    productId: str
+    locationName: str
+    rating: int
+    userNotes: str
 
-@app.get("/ratings")
-def get_product(productId: Optional[str] = None):
-    return f"hello everyone"
+@app.post("/CreateRating")
+def post_rating(rating: Rating):
+    inserted_rating = rating_container.create_item(body={
+        "id": str(uuid.uuid4()),
+        "timestamp": int(time.time()),
+        **dict(rating)
+    })
+    return JSONResponse(content=inserted_rating)
+
+
+@app.get("/GetRating")
+@app.get("/GetRating/{ratingId}")
+def get_rating_by_id(ratingId: Optional[str] = None):
+    if not ratingId:
+        return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content={"error": "Missing mandatory parameter 'ratingId'"})
+    try:
+        rating = rating_container.read_item(item=ratingId, partition_key=ratingId)
+        return JSONResponse(content=rating)
+    except cosmos_exceptions.CosmosResourceNotFoundError:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"error": f"Rating not found for id '{ratingId}'"})
+
+
+@app.get("/GetRatings")
+@app.get("/GetRatings/{userId}")
+def get_ratings_by_user(userId: Optional[str] = None):
+    rating_list = list(rating_container.query_items(
+        query="SELECT * FROM rating WHERE rating.userId=@userId",
+        parameters=[
+            { "name":"@userId", "value": userId }
+        ],
+        enable_cross_partition_query=True
+    ))
+    return JSONResponse(content=rating_list)
 
